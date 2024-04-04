90b3d3e43c056c5f452540aab16ce8252768c5b3
ferbianapuspita <ferbianapuspita02@gmail.com>
2024-04-04 13:44:23 +0700

    Tugas 9

---
+++ OOP/ape.php
@@ -0,0 +1,11 @@
+<?php
+
+class Ape extends Animal{
+    public $name;
+    public $legs = 4;
+    public $cold_blooded = "no";
+    public $yell = ("Auooo");
+
+
+}
+?>
---
+++ OOP/frog.php
@@ -0,0 +1,11 @@
+<?php
+
+class Frog extends Animal{
+    public $name;
+    public $legs = 4;
+    public $cold_blooded = "no";
+    public $jump = ("Hop Hop");
+
+
+}
+?>
